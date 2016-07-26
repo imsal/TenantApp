7f01fffffe1dee3bcfb7d0136cb7a0914c40eed0
Sal Tardibuono <s.tardibuono@live.com>
2016-07-25 19:14:47 -0700

    Added Building controllers, models and initialized suite controller

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,25 +1,44 @@
 class BuildingsController < ApplicationController
 
+  before_action :set_building, only: [:show, :edit, :update, :destroy]
 
   def new
-
+    @building = Building.new
   end
 
   def index
-
   end
 
   def show
-
   end
 
   def edit
   end
 
   def create
+    @building = Building.new(building_params)
+
+    respond_to do |format|
+      if @building.save
+        format.html { redirect_to @building, notice: 'Building was successfully created.' }
+        format.json { render :show, status: :created, location: @building }
+      else
+        format.html { render :new }
+        format.json { render json: building.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @building.update(building_params)
+        format.html { redirect_to @building, notice: 'Building was successfully updated.' }
+        format.json { render :show, status: :ok, location: @building }
+      else
+        format.html { render :edit }
+        format.json { render json: @building.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
@@ -28,11 +47,11 @@ class BuildingsController < ApplicationController
   private
 
   def building_params
-    params.require(:building).permite(:id, :property_id, :name, :address, :city, :state, :zip_code)
+    params.require(:building).permit(:id, :property_id, :name, :address, :city, :state, :zip_code)
   end
 
   def set_building
-    @building =
+    @building = Building.find(params[:id])
   end
 
 end
---
+++ app/controllers/suites_controller.rb
@@ -0,0 +1,2 @@
+class SuitesController < ApplicationController
+end
